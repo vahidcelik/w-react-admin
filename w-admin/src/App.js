6578921edf88ca36775fe5218a3a3ec4891906d4
Vahid Çelik <vahidcelik@gmail.com>
2020-04-11 01:46:51 +0300

    Using a Custom Home Page (Dashboard)

--- w-admin/src/App.js
+++ w-admin/src/App.js
@@ -6,10 +6,11 @@ import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
