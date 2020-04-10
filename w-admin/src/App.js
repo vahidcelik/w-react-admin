2b2eaebd73d1dacd11ab207c03fa367779f17120
Vahid Çelik <vahidcelik@gmail.com>
2020-04-10 23:18:04 +0300

    Use custom post components instead of ListGuesser

--- w-admin/src/App.js
+++ w-admin/src/App.js
@@ -1,13 +1,14 @@
 // in src/App.js
 import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
+import { PostList } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} />
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
