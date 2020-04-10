08a30fcd30cac65ef27af27758dfd82cac3485ab
Vahid Çelik <vahidcelik@gmail.com>
2020-04-10 21:43:15 +0300

    Mapping API Endpoints With Resources

--- w-admin/src/App.js
+++ w-admin/src/App.js
@@ -1,9 +1,13 @@
 // in src/App.js
 import React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
