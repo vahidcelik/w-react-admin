9797c39662575a8ffd7c2286d7b598f25552dfba
Vahid Çelik <vahidcelik@gmail.com>
2020-04-11 02:03:48 +0300

    Supporting Mobile Devices

--- w-admin/src/posts.js
+++ w-admin/src/posts.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
+import { useMediaQuery } from '@material-ui/core';
+import { List, SimpleList, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin';
 import { Edit, Create, Filter, SimpleForm, TextInput, ReferenceInput, SelectInput } from 'react-admin';
 
 const PostTitle = ({ record }) => {
     return <span>Post {record ? `"${record.title}"` : ''}</span>;
 };
 
-export const PostList = props => (
-    <List filters={<PostFilter />} {...props}>
-        <Datagrid>
-            <TextField source="id" />
-            <ReferenceField source="userId" reference="users">
-                <TextField source="name" />
-            </ReferenceField>
-            <TextField source="title" />
-            <TextField source="body" />
-            <EditButton />
-        </Datagrid>
-    </List>
-);
+export const PostList = (props) => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List {...props}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.title}
+                    secondaryText={record => `${record.views} views`}
+                    tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+                />
+            ) : (
+                    <Datagrid>
+                        <TextField source="id" />
+                        <ReferenceField label="User" source="userId" reference="users">
+                            <TextField source="name" />
+                        </ReferenceField>
+                        <TextField source="title" />
+                        <TextField source="body" />
+                        <EditButton />
+                    </Datagrid>
+                )}
+        </List>
+    );
+}
 
 export const PostEdit = props => (
     <Edit title={<PostTitle />} {...props}>
