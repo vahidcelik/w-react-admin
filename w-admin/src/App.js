7f4c57233f03d03d671720225d0b945e3995b7ab
Vahid Çelik <vahidcelik@gmail.com>
2020-04-11 00:38:50 +0300

    work with editGuesser

--- w-admin/src/App.js
+++ w-admin/src/App.js
@@ -1,6 +1,6 @@
 // in src/App.js
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, EditGuesser } from 'react-admin';
 import { PostList } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
@@ -8,7 +8,7 @@ import jsonServerProvider from 'ra-data-json-server';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} edit={EditGuesser} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
