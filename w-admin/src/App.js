d90c87a49f1e60a7df698ec94f2fb5f51591b0c8
Vahid Çelik <vahidcelik@gmail.com>
2020-04-11 00:41:16 +0300

    Adding Creation and Editing Capabilities

--- w-admin/src/App.js
+++ w-admin/src/App.js
@@ -1,14 +1,14 @@
 // in src/App.js
 import React from 'react';
 import { Admin, Resource, EditGuesser } from 'react-admin';
-import { PostList } from './posts';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={EditGuesser} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
--- w-admin/src/posts.js
+++ w-admin/src/posts.js
@@ -13,3 +13,28 @@ export const PostList = props => (
         </Datagrid>
     </List>
 );
+
+export const PostEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
+);
