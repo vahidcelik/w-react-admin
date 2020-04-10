79ba286d70ad56ae3f92476ce6c21a3dd02e66b0
Vahid Çelik <vahidcelik@gmail.com>
2020-04-10 23:18:44 +0300

    use user of name in post page

--- w-admin/src/posts.js
+++ w-admin/src/posts.js
@@ -5,7 +5,7 @@ export const PostList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
             <ReferenceField source="userId" reference="users">
-                <TextField source="id" />
+                <TextField source="name" />
             </ReferenceField>
             <TextField source="id" />
             <TextField source="title" />
